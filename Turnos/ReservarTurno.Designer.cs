768b02d449db7daf64aa22395559550e93928254
jonathans0ramirez <jonathans.ramirez@udea.edu.co>
2019-10-28 18:20:19 -0500

    nombre button reservar

--- Turnos/ReservarTurno.Designer.cs
+++ Turnos/ReservarTurno.Designer.cs
@@ -44,7 +44,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(97, 30);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button";
+            this.button1.Text = "Reservar";
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.Button1_Click);
             // 
